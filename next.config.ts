25f132693a67f31214f82b41dc528350139325a9
razor950 <razor950@gmail.com>
2025-04-11 03:53:45 -0400

    pushed changes to pages branch

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import createMDX from '@next/mdx'
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  output: 'standalone'
+  images: {
+    unoptimized: true,
+  },
+  output: 'export'
 };
 
 const withMDX = createMDX({
